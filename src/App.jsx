494ab37d03de136b7f53d40c7c9c2c00fb464cbb
Ayush Rohan <ayushrohan5@gmail.com>
2025-03-26 14:53:31 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Programs from './components/programs';
+import Programs from './components/Programs';
 import reviews from "./data";
 import Testimonial from "./components/Testimonial"
 import Statistics from './components/Statistics';
